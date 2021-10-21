f11ba626067cc9f6fb0f2b7cbf68190e2d372956
Kara <palumbokara@yahoo.com>
2021-10-21 13:08:31 -0600

    updated main page

---
+++ src/Heading.js
@@ -0,0 +1,8 @@
+export const Heading = () => {
+  return (
+    <div className="heading-text">
+      <h1>Welcome to Jobly</h1>
+      <p>A convenient place to find and apply to jobs!</p>
+    </div>
+  );
+};
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,27 +1,33 @@
 import React, { useContext } from "react";
 import UserContext from "./UserContext";
 import { Link } from "react-router-dom";
+import { Heading } from "./Heading";
 import "./Homepage.css";
 
 function Homepage() {
   const { currentUser } = useContext(UserContext);
 
   return (
-    <div className="homepage">
-      <h1>Jobly</h1>
-      <p>All the jobs in one, convenient place.</p>
+    <div className="homepage-container">
+      <div className="homepage">
+        <img
+          src="https://image.freepik.com/free-vector/people-applying-vacant-job-business-competition-available-vacancy-advertisement-position-application-competing-workers-cartoon-characters_335657-1596.jpg"
+          className="img"
+          alt="Man, woman, vacant illustration"
+        ></img>
+      </div>
+      <Heading />
       {currentUser ? (
         <h2>Welcome back, {currentUser.firstName || currentUser.username}!</h2>
       ) : (
-        <>
-          <div>
-            <Link to="/login">Log in</Link>
-          </div>
-
-          <div>
-            <Link to="/signup">Sign up</Link>
-          </div>
-        </>
+        <div className="login-signup">
+          <Link to="/login">
+            <button className="register-btn">Register</button>
+          </Link>
+          <Link to="/signup">
+            <button className="signup-btn">Signup</button>
+          </Link>
+        </div>
       )}
     </div>
   );
