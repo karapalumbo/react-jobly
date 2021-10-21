684931b9f1b2d8651f3a9166f857ea538d71ffbb
Kara <palumbokara@yahoo.com>
2021-10-21 13:59:35 -0600

    updated login and signup forms

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { useHistory } from "react-router-dom";
+import "./LoginForm.css";
 
 const LoginForm = ({ login }) => {
   const history = useHistory();
@@ -28,30 +29,34 @@ const LoginForm = ({ login }) => {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <FormGroup>
-        <Label for="username">Username</Label>
-        <Input
-          type="username"
-          name="username"
-          id="username"
-          placeholder="Enter your username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="=password">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Enter your password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <Button>Login</Button>
+    <Form className="login-form" onSubmit={handleSubmit}>
+      <h1>Welcome Back!</h1>
+      <h3>Login below to access your account.</h3>
+      <div className="input-container">
+        <FormGroup>
+          <Label for="username">Username</Label>
+          <Input
+            type="username"
+            name="username"
+            id="username"
+            placeholder="Enter your username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="=password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </FormGroup>
+      </div>
+      <Button className="login-btn">Login</Button>
     </Form>
   );
 };
--- src/forms/SignupForm.js
+++ src/forms/SignupForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { useHistory } from "react-router-dom";
+import "./SignupForm.css";
 
 const SignupForm = ({ signup }) => {
   const history = useHistory();
@@ -31,63 +32,77 @@ const SignupForm = ({ signup }) => {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <FormGroup>
-        <Label for="username">Username</Label>
-        <Input
-          type="username"
-          name="username"
-          id="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="=password">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="=firstName">First Name</Label>
-        <Input
-          type="text"
-          name="firstName"
-          id="firstName"
-          placeholder="First name"
-          value={formData.firstName}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="=lastName">Last Name</Label>
-        <Input
-          type="text"
-          name="lastName"
-          id="lastName"
-          placeholder="Last name"
-          value={formData.lastName}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="=email">Email</Label>
-        <Input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <Button>Signup</Button>
+    <Form className="signup-form" onSubmit={handleSubmit}>
+      <h1>Welcome to Jobly.</h1>
+      <h3>Your dream job is waiting!</h3>
+      <div className="signup-container">
+        <FormGroup>
+          <Label for="username" className="label">
+            Username
+          </Label>
+          <Input
+            type="username"
+            name="username"
+            id="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="=password" className="label">
+            Password
+          </Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="=firstName" className="label">
+            First Name
+          </Label>
+          <Input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="First name"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="=lastName" className="label">
+            Last Name
+          </Label>
+          <Input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Last name"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="=email" className="label">
+            Email
+          </Label>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </FormGroup>
+      </div>
+      <Button className="signup-btn">Signup</Button>
     </Form>
   );
 };
